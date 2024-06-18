7957d02ea6be8ba4258ce8048fa54c36b1cf407f
dee3-pika <146899185+dee3-pika@users.noreply.github.com>
2024-06-18 20:13:03 +0545

    first commit

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -8,10 +8,7 @@ import PopularCategories from "./PopularCategories";
 import PopularCompanies from "./PopularCompanies";
 
 const Home = () => {
-  const { isAuthorized } = useContext(Context);
-  if (!isAuthorized) {
-    return <Navigate to={"/login"} />;
-  }
+ 
   return (
     <>
       <section className="homePage page">
