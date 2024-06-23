e84f0927c6969cc250cf3cf4b1046cf0829f773b
dee3-pika <146899185+dee3-pika@users.noreply.github.com>
2024-06-23 20:32:53 +0545

    fourth commit

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -6,12 +6,18 @@ import HeroSection from "./HeroSection";
 import HowItWorks from "./HowItWorks";
 import PopularCategories from "./PopularCategories";
 import PopularCompanies from "./PopularCompanies";
+import Navbar from "../Layout/Navbar";
 
 const Home = () => {
+  const { isAuthorized } = useContext(Context);
+  if (!isAuthorized) {
+    return <Navigate to={"/login"} />;
+  }
  
   return (
     <>
       <section className="homePage page">
+        <Navbar/>
         <HeroSection />
         <HowItWorks />
         <PopularCategories />
